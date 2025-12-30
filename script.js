51317ef74957e63aaf6476103d6cb13fcde73f21
NIKHIL-bytes <nikhil.official10001111@gmail.com>
2025-12-30 23:24:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// ================= MOBILE NAV TOGGLE =================
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
