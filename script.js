2b94a924b8860e7902892a122b7a69cc1b1a0933
NIKHIL-bytes <nikhil.official10001111@gmail.com>
2025-12-31 16:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+// ================= CERTIFICATE POPUP =================
+const certs = document.querySelectorAll(".cert");
+const certModal = document.getElementById("cert-modal");
+const certModalImg = document.getElementById("cert-modal-img");
+
+certs.forEach(cert => {
+    cert.addEventListener("click", () => {
+        const imgSrc = cert.getAttribute("data-img");
+        certModalImg.src = imgSrc;
+        certModal.classList.add("active");
+        document.body.style.overflow = "hidden"; // stop scroll
+    });
+});
+
+// Close when clicking outside image
+certModal.addEventListener("click", (e) => {
+    if (e.target === certModal) {
+        certModal.classList.remove("active");
+        certModalImg.src = "";
+        document.body.style.overflow = "auto";
+    }
+});
+document.addEventListener("contextmenu", (e) => {
+    if (e.target.tagName === "IMG") e.preventDefault();
+});
